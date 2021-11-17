79481382d6d1e07d098ca5a04cc34523d72736b1
worawawa <89581419+worawawa@users.noreply.github.com>
2021-11-17 16:45:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def event_handle(event):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        replyObj = TextSendMessage(text=ก็มาดิอู้กๆ)
+        replyObj = TextSendMessage(text="ก็มาดิอู้กๆ")
         line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
             replyObj = TextSendMessage(text=msg)
